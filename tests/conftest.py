d23172cea708e1ff6d887ece7bc3000990f2a71e
hmasdev <hmasuidev@gmail.com>
2024-09-07 20:50:37 +0900

    Add fixture to load API keys from environment variables

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+import pytest
+from werewolf.const import SERVICE_APIKEY_ENVVAR_MAP
+
+
+@pytest.fixture(scope='session', autouse=True)
+def api_keys():
+    for envvar in SERVICE_APIKEY_ENVVAR_MAP.values():
+        os.environ[envvar] = os.getenv(envvar)
+    load_dotenv()
+    yield
